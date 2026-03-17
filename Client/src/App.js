de797fb4c6bdbdbd7884a178f7e5ea4ab2c60aac
Edwin Mwenda Mwiti <eduedwyn5@gmail.com>
2026-03-18 00:39:27 +0300

    Initial commit 1

---
+++ Client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Agreement from './pages/Agreement';
+import Profile from './pages/Profile';
+import { useUser } from './context/UserContext';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  const { user } = useUser();
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+          <Route path="/agreement/:id?" element={user ? <Agreement /> : <Navigate to="/login" />} />
+          <Route path="/profile" element={user ? <Profile /> : <Navigate to="/login" />} />
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      </main>
+      <Footer />
+      <Toaster position="top-right" />
+    </div>
+  );
+}
+
+export default App;
